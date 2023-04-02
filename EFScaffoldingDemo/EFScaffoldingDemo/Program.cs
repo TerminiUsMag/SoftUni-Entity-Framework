320ec4d4188b6b614608d5ebaa3417704aff46b6
TerminiUsMag <nikolaidan1@abv.bg>
2023-04-02 15:23:25 +0300

    Added Scaffolding DEMO

---
+++ EFScaffoldingDemo/EFScaffoldingDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using EFScaffoldingDemo;
+using Microsoft.EntityFrameworkCore;
+
+using (SoftUniContext context = new SoftUniContext())
+{
+    var employees = await context.Employees.OrderByDescending(e => e.Salary).Where(e => e.DepartmentId == 7).ToListAsync();
+
+    foreach (var employee in employees)
+    {
+        Console.WriteLine($"{employee.FirstName} {employee.LastName} - {employee.Salary} -> {employee.Department?.Name}");
+    }
+}
